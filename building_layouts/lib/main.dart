422ffc4bb10103b71519dfe986ca6720c5aacc3e
Nicolas Terrien <nicolas.terrien@imt-atlantique.net>
2020-04-01 16:26:39 +0200

    Step 5

--- building_layouts/lib/main.dart
+++ building_layouts/lib/main.dart
@@ -80,7 +80,17 @@ class MyApp extends StatelessWidget {
             title: Text('Flutter layout demo'),
           ),
           body: Column(
-            children: [titleSection, buttonSection, textSection],
+            children: [
+              Image.asset(
+                'images/lake.jpg',
+                width: 600,
+                height: 240,
+                fit: BoxFit.cover,
+              ),
+              titleSection,
+              buttonSection,
+              textSection
+            ],
           ),
         ));
   }
