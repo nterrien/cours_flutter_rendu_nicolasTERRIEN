859763457df2c4b8ac9a43bb834080fb7925591e
Nicolas Terrien <nicolas.terrien@imt-atlantique.net>
2020-04-02 15:23:18 +0200

    Étape 5 du 2nd codelab : Ajout de ce qui permet d'ecrire un message

--- building_beautiful_ui/lib/main.dart
+++ building_beautiful_ui/lib/main.dart
@@ -14,11 +14,52 @@ class FriendlychatApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => new ChatScreenState();
+}
+
+class ChatScreenState extends State<ChatScreen> {
+  final TextEditingController _textController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(title: new Text("Friendlychat")),
+      body: _buildTextComposer(),
     );
+  }
+
+  Widget _buildTextComposer() {
+    return new IconTheme(
+        data: new IconThemeData(color: Theme.of(context).accentColor),
+        child: new Container(
+          margin: const EdgeInsets.symmetric(horizontal: 8.0),
+          child: new Row(
+            children: <Widget>[
+              new Flexible(
+                child: new TextField(
+                  controller: _textController,
+                  onSubmitted: _handleSubmitted,
+                  decoration:
+                      new InputDecoration.collapsed(hintText: "Send a message"),
+                ),
+              ),
+              new Container(
+                //new
+                margin: new EdgeInsets.symmetric(horizontal: 4.0), //new
+                child: new IconButton(
+                    //new
+                    icon: new Icon(Icons.send), //new
+                    onPressed: () =>
+                        _handleSubmitted(_textController.text)), //new
+              ),
+            ],
+          ),
+        ));
+  }
+
+  void _handleSubmitted(String text) {
+    _textController.clear();
   }
 }
